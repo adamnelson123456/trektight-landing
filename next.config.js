006ef94681b1a3e804faf55a476893a8d56d0326
Adam Nelson <adamnelson@Adams-MBP.attlocal.net>
2025-03-27 14:02:06 -0500

    Add Cloudinary player domain to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 'player.cloudinary.com'],
+  },
+}
+
+module.exports = nextConfig 
